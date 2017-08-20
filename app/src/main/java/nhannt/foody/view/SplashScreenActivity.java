0530893e9ded54eb271ee5f63af16cf1861ad1f7
NhanNT <nhannt.bk@gmail.com>
2017-08-20 14:14:05 +0700

    Init project

---
+++ app/src/main/java/nhannt/foody/view/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package nhannt.foody.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nhannt.foody.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
