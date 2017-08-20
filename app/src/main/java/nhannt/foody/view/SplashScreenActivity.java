54aae90bba3d2bab62686366a2d4025e79843569
NhanNT <nhannt.bk@gmail.com>
2017-08-20 15:03:17 +0700

    Create Splash Screen

--- app/src/main/java/nhannt/foody/view/SplashScreenActivity.java
+++ app/src/main/java/nhannt/foody/view/SplashScreenActivity.java
@@ -1,14 +1,30 @@
 package nhannt.foody.view;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import nhannt.foody.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private TextView mTvVersion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        setupViews();
+    }
+
+    private void setupViews() {
+        mTvVersion = (TextView) findViewById(R.id.tv_version);
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            mTvVersion.setText(getString(R.string.version_splash, packageInfo.versionName));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
